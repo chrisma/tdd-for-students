c2da6642322a450b84e73fba44b300cba229acab
Christoph Matthies <christoph.matthies@gmail.com>
2016-05-02 16:13:13 +0200

    Print current branch before exit.

--- pull_tasks.py
+++ pull_tasks.py
@@ -1,30 +1,51 @@
-import logging
-from sh import git, ErrorReturnCode, ErrorReturnCode_128
+import logging, os
+from sh import git, ErrorReturnCode, ErrorReturnCode_1, ErrorReturnCode_128
 from sys import exit
 
 logging.basicConfig(level=logging.DEBUG) #logging.WARNING
 logging.getLogger('sh').setLevel(logging.WARNING)
+log = logging.getLogger(__name__)
 
-repo_path = "."
 remote_name = "tasks"
 remote_url_https = "https://github.com/hpi-swt2-exercise/java-tdd-challenge-tasks.git"
 remote_url_ssh = "git@github.com:hpi-swt2-exercise/java-tdd-challenge-tasks.git"
-local_branch = "master"
+repo_path = os.getcwd()
 
-logging.debug("Attempting to add git remote...")
+log.debug("Running git commands in {path}".format(path=repo_path))
+
+log.debug("Attempting to add git remote...")
 try:
 	remote_add = git("-C", repo_path, "remote", "add", remote_name, remote_url_ssh)
-	logging.debug("Added remote '{remote}'".format(remote=remote_name))
+	log.debug("Added remote '{remote}'".format(remote=remote_name))
 except ErrorReturnCode_128:
-	logging.debug("Remote '{remote}' already exists".format(remote=remote_name))
+	log.debug("Remote '{remote}' already exists".format(remote=remote_name))
 except ErrorReturnCode:
-    logging.error("Error when adding '{remote}' remote".format(remote=remote_name))
-    exit(1)
+	log.error("Unknown error when adding '{remote}' remote".format(remote=remote_name))
+	exit(1)
 
-logging.debug("Pulling remote '{remote}', overwriting local changes...".format(remote=remote_name))
+log.debug("Attempting to extract current branch...")
 try:
-	git("-C", repo_path, "pull", "-X", "theirs", remote_name, local_branch)
+	current_branch = git("-C", repo_path, "symbolic-ref", "--short", "-q", "HEAD")
+	current_branch = current_branch.strip('\n')
+	log.debug("Currently on branch '{branch}'".format(branch=current_branch))
+except ErrorReturnCode_1:
+	log.error("Error when checking for current branch. Probably in 'detached HEAD' mode")
+	exit(1)
 except ErrorReturnCode:
-	logging.error("Error pulling from remote '{remote}'".format(remote=remote_name))
+	log.error("Unknown error when checking for current branch")
+	exit(1)
+
+log.debug("Pulling remote '{remote}' into branch {branch}, overwriting local changes...".format(
+	remote=remote_name, branch=current_branch))
+try:
+	git("-C", repo_path, "pull", "-X", "theirs", remote_name, current_branch)
+except ErrorReturnCode:
+	log.error("Error pulling from remote '{remote}'".format(remote=remote_name))
+
+try:
+	latest_commit = git("rev-parse", "HEAD").strip('\n')
+	log.debug("Latest commit is now {hash}".format(hash=latest_commit))
+except ErrorReturnCode:
+	log.error("Error getting the lastest commit")
 
 # import pdb; pdb.set_trace()
